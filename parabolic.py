648ae8eaae522e66ded4f84aa868fcff30b6e75a
Gabe <finkegabriel@gmail.com>
2020-10-23 22:01:26 -0700

    update

--- parabolic.py
+++ parabolic.py
@@ -7,11 +7,11 @@ g = -9.81
 def x_distance():
     return 0
 
+#this will calc. z values for the parabola by taking the backwards order of the array given
 def z(x):
     a = []
     for l in range(0,len(x)):
-        s = 0-x[l]
-        a.append(s)
+        a.append(0-(len(x)-l))
     return a
         
 def X(init,final):
@@ -25,14 +25,12 @@ def parabolic(a,b,c,n):
     final = []
     for l in n:
         final.append(((-(a*(l))**2))+(b*l)+c)
-    print("y values ",final)
     return final
 
 def line(m,b,n):
     #y=mx+b
     final = []
     for l in range(0,n):
-        print("n ",l)
         final.append(m*(l)+b)
     return final
 
@@ -45,5 +43,4 @@ def trajectory(x,y,z):
 def finalX(angle,velocity):
     #l = math.pow(velocity,2)+2g(x[0]-x[1])
     a = (math.cos(math.radians(angle))*velocity)
-    print("projectile ",a)
     return 0
