989e624e6e427d47e366c4152b05ac3bc911885a
AGLT33\finke <gabriel.finke@activeg.com>
2022-07-30 17:05:26 -0700

    wrote a quick midpoint calculation

--- main.py
+++ main.py
@@ -18,6 +18,12 @@ doex = [.4,.4,.4,.4]
 doey = [11,2,2,11]
 doez = [9.9,9.9,15.9,15.9]
 
+#draw a paraobola along the x,z (side view) axis 
+#draw a parabola along the y,z (front view) axis
+#(ax^2+bx+c)
+#x=(-b/2a)
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      
+
 def paraBolEqn(data,a,b,c,d):
     x,y = data
     return -(((x-b)/a)**2+((y-d)/c)**2)+1.0
@@ -26,15 +32,13 @@ popt,pcov=optimize.curve_fit(paraBolEqn,np.vstack((doex,doey)),doez,p0=[1.5,0.4,
 # x, y = np.meshgrid(np.linspace(np.min(doex), np.max(doex),1), np.linspace(np.min(doey),np.max(doey), 1))
 # ax.plot_wireframe(x, y, paraBolEqn((x,y), *popt))
 ax.scatter(doex, doey, doez, color='b')
-print(doex,doey,doez)
+ax.scatter(doex[0],((doey[0]+doey[1])/2),((doez[0]+doez[2])/2),color='b')
+print(((doey[0]+doey[1])/2),((doez[0]+doez[2])/2))
 
 guess = (1,1)
 #draw hoop, arrays are for x, y location on the graph
 backboard=[5,11.5]
 xy=[2,10]
-
-
-
 #hoop location parameters
 circlexy=[2.25,6.5]
 x = para.X(0,15)
