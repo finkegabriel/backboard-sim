0dc80f81321baf6827f5d53f4aea5b4a872d2d12
AGLT33\finke <gabriel.finke@activeg.com>
2022-08-07 17:20:34 -0700

    update

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ doez = [9.9,9.9,15.9,15.9]
 theta = 1.52
 h= 0
 k= 0
+offset = .4
 
 xParaBounds = 9
 yParaBounds = 7
@@ -42,13 +43,13 @@ yi = .03*xi**2
 yii = (((-xi*math.cos(theta)+yi*math.sin(theta)-h)**2)+k)
 
 #plot of the top parabola to average with a matrix
-ax.plot(yii+.4,-xi+((doey[0]+doey[1])/2),(doez[2]),zdir='z',color='green')
+ax.plot(yii+offset,-xi+((doey[0]+doey[1])/2),(doez[2]),zdir='z',color='green')
 
 # second plot of the parabola off to the side of the hoop
 xii = np.linspace(-5.5,6.5,500)
 yii = .03*xii**2
 yiii = (((-xi*math.cos(theta)+yi*math.sin(theta)-h)**2)+k)
-ax.plot(yii+.4,xi+((doez[0]+doez[2])/2),((doey[0])),zdir='y',color='green')#(doey[0]),zdir='y')
+ax.plot(yii+offset,xi+((doez[0]+doez[2])/2),((doey[0])),zdir='y',color='green')#(doey[0]),zdir='y')
 
 xiii = np.linspace(-5.5,6.5,500)
 yiiii = .03*xii**2
@@ -57,7 +58,7 @@ yiiiii = (((-xi*math.cos(theta)+yi*math.sin(theta)-h)**2)+k)
 ########
 xip = np.linspace(0, 10, 500)
 yip = .09*-xip**2+12
-ax.plot(xip+.4,yip+1.5,10,zdir='y',color='black')
+ax.plot(xip+offset,yip+1.5,10,zdir='y',color='black')
 ########
 
 # print("x ",doex[0],doex[1],doex[2],doex[3])
@@ -87,24 +88,43 @@ c = Circle((circlexy),radius=2.25,fill=False)
 #72 - 9 48 - 6 
 p = Rectangle(xy,9,6,edgecolor='red',facecolor='none')
 
-print(" x ",xip+.4," y ",yip+1.5," z ",10," length ",xip[len(xip)-1],yip[len(yip)-1],10)
-
 # to get vertex of the parabola index at 0 so xip[len(xip)-(len(xip))],yip[len(yip)-(len(yip))]+OFFSET will result in locating index 0
 # use this in automated fashion to get the vertex and calculate bounce angle
 
 ## player stats
-ax.scatter(xip[len(xip)-(len(xip))]+.4,yip[len(yip)-(len(yip))]+1.5,10,zdir='y',color='purple')
+ax.scatter(xip[len(xip)-(len(xip))]+offset,yip[len(yip)-(len(yip))]+1.5,10,zdir='y',color='purple')
 playerHeight = (yip[len(yip)-1]-yip[len(yip)-1]+1.5)/2
 print("height ",playerHeight)
-ax.scatter(xip[len(xip)-1]+.4,playerHeight,10,zdir='y',color='purple')
-playerPOS = [xip[len(xip)-1]+.4,yip[len(yip)-1]+1.5,10]
+ax.scatter(xip[len(xip)-1]+offset,playerHeight,10,zdir='y',color='purple')
+playerPOS = [xip[len(xip)-1]+offset,yip[len(yip)-1]+1.5,10]
 ##
 
+# middle point to aim for bounce trajectory
+ax.scatter(circlexy[0],circlexy[1],10,zdir='z',color='purple')
+
+#calculate bounce for 1 situation
+x1 = circlexy[0]
+y1 = circlexy[1]
+x2 = xip[len(xip)-(len(xip))]+offset
+y2 = yip[len(yip)-(len(yip))]+1.5
+
+M=(y2-y1)/(x2-x1)
+y=(M*xip)+3.5
+
+ax.set_xlim(x1,x2)
+ax.set_ylim(y1,y2)
+ax.plot(xip+offset,y+10,y+13.8,zdir='z',color='purple')
+print(M)
+
+# print(x1,y1,x2,y2)
+# plt.plot(x1,y1,marker='o')
+# plt.plot(x2,y2,marker='x')
+
 for oX in range(xParaBounds):
-    ax.plot(yiiii+.4,xiii+((doez[0]+doez[2])/2),((doey[1])+oX),zdir='y',color='green')
+    ax.plot(yiiii+offset,xiii+((doez[0]+doez[2])/2),((doey[1])+oX),zdir='y',color='green')
 
 for oY in range(yParaBounds):
-    ax.plot(yi+.4,-xi+((doey[0]+doey[1])/2),(doez[2]-oY),zdir='z',color='green')
+    ax.plot(yi+offset,-xi+((doey[0]+doey[1])/2),(doez[2]-oY),zdir='z',color='green')
 
 g1.draw(ax,b,p,c)
 plt.show()
